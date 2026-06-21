68abaab5bb2493f899ae1f8756cf11acee9102e6
Adamin88 <32130262+Adamin88@users.noreply.github.com>
2026-06-21 08:39:42 +0000

    1.1

--- chapter1/1.1/log_output/src/main/java/com/example/log_output/LogLogger.java
+++ chapter1/1.1/log_output/src/main/java/com/example/log_output/LogLogger.java
@@ -6,13 +6,11 @@ import java.util.UUID;
 
 @Component
 public class LogLogger {
-    // Generated firmly inside memory on startup
     private final String randomString = UUID.randomUUID().toString();
 
-    // Runs every 5000 milliseconds (5 seconds)
     @Scheduled(fixedRate = 5000)
     public void logOutput() {
-        // Bypasses logging libraries to print cleanly to standard console out
+
         System.out.println(randomString);
     }
 }
